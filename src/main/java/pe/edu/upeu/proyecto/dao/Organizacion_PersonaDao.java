a80979354111835aac536dfce396ffe6a135c4af
Usuario <Usuario@DESKTOP-OE8V1MC>
2020-12-03 11:56:00 -0500

    DaoAñamuro

---
+++ src/main/java/pe/edu/upeu/proyecto/dao/Organizacion_PersonaDao.java
@@ -0,0 +1,8 @@
+package pe.edu.upeu.proyecto.dao;
+
+import java.util.Map;
+
+public interface Organizacion_PersonaDao {
+	Map< String , Object >  readAll();
+
+}
---
+++ src/main/java/pe/edu/upeu/proyecto/dao/ResolucionDao.java
@@ -0,0 +1,8 @@
+package pe.edu.upeu.proyecto.dao;
+
+import java.util.Map;
+
+public interface ResolucionDao {
+	Map< String , Object >  readAll();
+
+}
